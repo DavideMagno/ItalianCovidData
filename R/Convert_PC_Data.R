03a1d715940fb04b138f31a0e9affcd8511cb8be
Davide Magno <davide.magno@curiousfrm.com>
2020-05-04 18:58:20 +0000

    Updated to facilate automation

--- R/Convert_PC_Data.R
+++ R/Convert_PC_Data.R
@@ -36,6 +36,15 @@ CalculateIncrement <- function(data, fields) {
   return(inc.data)
 }
 
+setwd("/home/davide_magno/ItalyCovid19/")
+
+## Get last update
+
+last.update <- "https://raw.githubusercontent.com/DavideMagno/ItalianCovidData/master/" %>% 
+  paste0("Daily_Covis19_Italian_Data_Cumulative.csv") %>% 
+  {readr::read_csv(url(.))} %>% 
+  {dplyr::last(.$Date)}
+
 ## Input data
 region.file <- "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv"
 region.fields <- c("data", "denominazione_regione", "ricoverati_con_sintomi", 
@@ -49,6 +58,8 @@ province.fields <- c("data", "denominazione_regione", "denominazione_provincia",
                      "totale_casi")
 province.new.fields <- c("Date", "Region", "Province", "Total Positive") 
 
+
+
 ## Run 
 
 Dati.ufficiali <- TreatOfficialData(region.file, region.fields, region.new.fields)
@@ -60,36 +71,60 @@ Dati.ufficiali.increment <- CalculateIncrement(Dati.ufficiali, "Region")
 Dati.ufficiali.province.increment <- CalculateIncrement(Dati.ufficiali.province, 
                                                         c("Region", "Province"))
 
-## Write Data 
-write.csv(Dati.ufficiali, "Daily Covis19 Italian Data Cumulative", 
-          row.names = FALSE)
-write.csv(Dati.ufficiali, "Daily_Covis19_Italian_Data_Cumulative.csv", 
-          row.names = FALSE)
-write.csv(Dati.ufficiali.increment, "Daily Covis19 Italian Data Incremental", 
-          row.names = FALSE)
-write.csv(Dati.ufficiali.increment, "Daily_Covis19_Italian_Data_Incremental.csv", 
-          row.names = FALSE)
-write.csv(Dati.ufficiali.province, "Daily_Covis19_Italian_Data_Province_Cumulative.csv", 
-          row.names = FALSE)
-write.csv(Dati.ufficiali.province.increment, "Daily_Covis19_Italian_Data_Province_Incremental.csv", 
-          row.names = FALSE)
+if (dplyr::last(Dati.ufficiali$Date) == last.update) {
+  message(paste(Sys.time(), "no update available"))
+} else {
+  message("Starting the update")
+  ## Write Data 
+  write.csv(Dati.ufficiali, "Daily Covis19 Italian Data Cumulative", 
+            row.names = FALSE)
+  write.csv(Dati.ufficiali, "Daily_Covis19_Italian_Data_Cumulative.csv", 
+            row.names = FALSE)
+  write.csv(Dati.ufficiali.increment, "Daily Covis19 Italian Data Incremental", 
+            row.names = FALSE)
+  write.csv(Dati.ufficiali.increment, "Daily_Covis19_Italian_Data_Incremental.csv", 
+            row.names = FALSE)
+  write.csv(Dati.ufficiali.province, "Daily_Covis19_Italian_Data_Province_Cumulative.csv", 
+            row.names = FALSE)
+  write.csv(Dati.ufficiali.province.increment, "Daily_Covis19_Italian_Data_Province_Incremental.csv", 
+            row.names = FALSE)
+  message("Performing Tests")
+  ## Tests
+  cum.total <- Dati.ufficiali %>% 
+    dplyr::filter(Date == dplyr::last(Dati.ufficiali$Date)) %>% 
+    dplyr::summarise_at(dplyr::vars(-Date, -Region), sum)
+  
+  inc.total <- Dati.ufficiali.increment %>% 
+    dplyr::summarise_at(dplyr::vars(-Date, -Region), sum)
+  
+  pro.cum.total <- Dati.ufficiali.province %>% 
+    dplyr::filter(Date == dplyr::last(Dati.ufficiali$Date)) %>% 
+    dplyr::summarise_at("Total Positive", sum)
+  
+  pro.inc.total <- Dati.ufficiali.province.increment %>% 
+    dplyr::summarise_at("Total Positive", sum)
+  
+  cross.total <- Dati.ufficiali %>% 
+    dplyr::filter(Date == dplyr::last(Dati.ufficiali$Date)) %>%
+    dplyr::summarise_if(is.numeric, sum) %>% 
+    {rowSums(.) - .$Tests}
+  
+  testthat::test_that("totals", testthat::expect_identical(cum.total, inc.total))
+  testthat::test_that("contagion1", 
+                      testthat::expect_identical(as.numeric(pro.cum.total), 
+                                                 cross.total))
+  testthat::test_that("contagion2", 
+                      testthat::expect_identical(as.numeric(pro.inc.total), 
+                                                 cross.total))
+  message("Writing Data")
+  system("git add 'Daily Covis19 Italian Data Cumulative'")
+  system("git add 'Daily Covis19 Italian Data Incremental'")
+  system("git add Daily_Covis19_Italian_Data_Cumulative.csv")
+  system("git add Daily_Covis19_Italian_Data_Incremental.csv")
+  system("git add Daily_Covis19_Italian_Data_Province_Cumulative.csv")
+  system("git add Daily_Covis19_Italian_Data_Province_Incremental.csv")
+  system(paste("git commit -m 'Data updated to the", Sys.Date(), "'"))
+  system("git push origin master")
+  message("Upload Completed")
+}
 
-## Tests
-Dati.ufficiali %>% 
-  dplyr::filter(Date == dplyr::last(Dati.ufficiali$Date)) %>% 
-  dplyr::summarise_at(dplyr::vars(-Date, -Region), sum)
-
-Dati.ufficiali.increment %>% 
-  dplyr::summarise_at(dplyr::vars(-Date, -Region), sum)
-
-Dati.ufficiali.province %>% 
-  dplyr::filter(Date == dplyr::last(Dati.ufficiali$Date)) %>% 
-  dplyr::summarise_at("Total Positive", sum)
-
-Dati.ufficiali.province.increment %>% 
-  dplyr::summarise_at("Total Positive", sum)
-
-Dati.ufficiali %>% 
-  dplyr::filter(Date == dplyr::last(Dati.ufficiali$Date)) %>%
-  dplyr::summarise_if(is.numeric, sum) %>% 
-  {rowSums(.) - .$Tests}
